0134cd164fd9e9ff9d3a4b4d032114c3829555f2
Fchirwa <F88kavutausiwa@gmail.com>
2013-05-15 16:52:35 +0200

    working on fine tunes in hypertension

---
+++ db/ccc_global_property.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.5.24, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: openmrs_chronic
+-- ------------------------------------------------------
+-- Server version	5.5.24-0ubuntu0.12.04.1-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `global_property`
+--
+
+DROP TABLE IF EXISTS `global_property`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `global_property` (
+  `property` varbinary(255) NOT NULL DEFAULT '',
+  `property_value` mediumtext,
+  `description` text,
+  `uuid` char(38) NOT NULL,
+  PRIMARY KEY (`property`),
+  UNIQUE KEY `global_property_uuid_index` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `global_property`
+--
+
+LOCK TABLES `global_property` WRITE;
+/*!40000 ALTER TABLE `global_property` DISABLE KEYS */;
+INSERT INTO `global_property` VALUES ('create.from.dde.server','true','','95401a0d-b304-11e2-83b6-0024be7f1be1'),('dde_server_ip','localhost:3001','',''),('dde_server_password','admin','','a9d2dac1-b304-11e2-83b6-0024be7f1be1'),('dde_server_username','admin','','a329228b-b304-11e2-83b6-0024be7f1be1');
+/*!40000 ALTER TABLE `global_property` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-05-03  9:31:51
