41a044f23156009215298d8eececa6fbf0b1e6ef
Fuvu Chirwa <fuvu.chirwa@gmail.com>
2013-07-11 17:08:19 +0200

    working on epilepsy workflow

--- app/controllers/protocol_patients_controller.rb
+++ app/controllers/protocol_patients_controller.rb
@@ -231,11 +231,11 @@ class ProtocolPatientsController < ApplicationController
     @circumference = Observation.find_by_sql("SELECT * from obs
                    WHERE concept_id = (SELECT concept_id FROM concept_name WHERE name = 'Head circumference' LIMIT 1) AND voided = 0
                    AND voided = 0 AND person_id = #{@patient.id} ORDER BY obs_datetime DESC LIMIT 1").first.value_numeric rescue nil
-    @diabetic = ConceptName.find_by_concept_id(Vitals.get_patient_attribute_value(@patient, "Patient has Diabetes")).name rescue []
+    @diabetic = ConceptName.find_by_concept_id(Vitals.get_patient_attribute_value(@patient, "Patient has Diabetes")).name rescue "unknown"
 
     @treatements_list = ["Heart disease", "Stroke", "TIA", "Diabetes", "Kidney Disease"]
 
-    @treatements_list.delete_if {|var| var == "Diabetes"} if @diabetic.upcase == "YES"
+      @treatements_list.delete_if {|var| var == "Diabetes"} if @diabetic.upcase == "YES"
     @current_program = current_program
 	end
 
@@ -310,7 +310,8 @@ class ProtocolPatientsController < ApplicationController
 	end
 
 	def clinic_visit
-    
+
+    current_date = (!session[:datetime].nil? ? session[:datetime].to_date : Date.today)
     @patient = Patient.find(params[:patient_id]) rescue nil
 
     redirect_to '/encounters/no_patient' and return if @patient.nil?
@@ -329,14 +330,23 @@ class ProtocolPatientsController < ApplicationController
     #@occupation = Vitals.occupation(@patient)
 
 		if @current_program == "EPILEPSY PROGRAM"
+      @regimen_concepts = MedicationService.hypertension_dm_drugs
       @first_visit = is_first_epilepsy_clinic_visit(@patient.id)
       @mrdt = Vitals.current_vitals(@patient, "RDT or blood smear positive for malaria") rescue nil
       unless @mrdt.blank?
         @mrdt = @mrdt.value_text.upcase rescue ConceptName.find_by_concept_id(@mrdt.value_coded).name.upcase
       end
+      
       if @first_visit == false
-        @in_seizure = Vitals.todays_vitals(Patient.find(params[:patient_id]), "Patient in active seizure")
+        concept = ConceptName.find_by_name('Patient in active seizure').concept_id
+        @in_seizure  = Observation.find_by_sql("SELECT * from obs where concept_id = '#{concept}' AND person_id = '#{@patient.id}'
+                    AND DATE(obs_datetime) = '#{current_date}' AND voided = 0
+                    ORDER BY  obs_datetime DESC, date_created DESC LIMIT 1").first rescue 0
+        
         @in_seizure = @in_seizure.value_text.upcase rescue ConceptName.find_by_concept_id(@in_seizure.value_coded).name.upcase rescue nil
+        #raise @in_seizure.to_yaml
+        #@in_seizure = Vitals.todays_vitals(Patient.find(params[:patient_id]), "Patient in active seizure")
+        #@in_seizure = @in_seizure.value_text.upcase rescue ConceptName.find_by_concept_id(@in_seizure.value_coded).name.upcase rescue nil
       end
       render :template => "/protocol_patients/epilepsy_clinic_visit"
 		end
@@ -382,7 +392,7 @@ class ProtocolPatientsController < ApplicationController
     @first_visit = is_first_hypertension_clinic_visit(@patient.id)
 		 occupation_attribute = PersonAttributeType.find_by_name("Occupation")
      @person_attribute = PersonAttribute.find(:first, :conditions => ["person_id = ? AND person_attribute_type_id = ?", @patient.person.id, occupation_attribute.person_attribute_type_id]).value rescue "Unknown"
-     
+     @person_attribute = "<span>   :Patient occupation is " + @person_attribute + "</span>"
     @current_program = current_program
 	end
 end
