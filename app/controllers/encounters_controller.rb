820d0febaa8ee0c9bdd48edfc2418159881c3dd7
Fuvu Chirwa <fuvu.chirwa@gmail.com>
2014-03-14 13:38:12 +0200

    Adding triggers for vitals

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -48,12 +48,13 @@ class EncountersController < ApplicationController
               )
 
             end
-
-            ProgramEncounterDetail.create(
-              :encounter_id => @encounter.id.to_i,
-              :program_encounter_id => @program_encounter.id,
-              :program_id => @program.id
-            )
+            if params[:encounter_type].upcase != "VITALS"
+                ProgramEncounterDetail.create(
+                  :encounter_id => @encounter.id.to_i,
+                  :program_encounter_id => @program_encounter.id,
+                  :program_id => @program.id
+                )
+            end
 						
             @current = PatientProgram.find_by_program_id(@program.id,
               :conditions => ["patient_id = ? AND COALESCE(date_completed, '') = ''", patient.id])
