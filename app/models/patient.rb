fd9f7b614eb2602ccc7a5e9b29ac6532ab2c2a6f
Fuvu Chirwa <fuvu.chirwa@gmail.com>
2014-03-25 10:20:56 +0200

    Adding dashed national id

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -79,4 +79,19 @@ class Patient < ActiveRecord::Base
  		status = status.squish
   end
 
+    def national_id_with_dashes
+    id = national_id
+    length = id.length
+    case length
+      when 13
+        id[0..4] + "-" + id[5..8] + "-" + id[9..-1] rescue id
+      when 9
+        id[0..2] + "-" + id[3..6] + "-" + id[7..-1] rescue id
+      when 6
+        id[0..2] + "-" + id[3..-1] rescue id
+      else
+        id
+    end
+  end
+
 end
